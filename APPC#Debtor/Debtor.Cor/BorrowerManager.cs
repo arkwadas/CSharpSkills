e1265f244cabad0c13437fb791a011c87da07cf7
arkwadas <ark.wadas@gmail.com>
2023-09-20 00:24:56 +0200

    Do ukonczenia
    Występuje mały bład w petli

---
+++ APPC#Debtor/Debtor.Cor/BorrowerManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Debtor.Core
+{
+    public class BorrowerManager
+    {
+        private List<Borrower> Borrowers { get; set; } 
+
+        public BorrowerManager()
+        {
+            Borrowers = new List<Borrower>();
+        }
+
+        public void AddBorrower(string name, decimal amount)
+        {
+            var borrower = new Borrower
+            {
+                Name = name,
+                Amount = amount
+            };
+
+            Borrowers.Add(borrower);
+        }
+
+        public void DeleteBorrower(string name)
+        {
+            foreach (var borrower in Borrowers)
+            {
+                if(borrower.Name == name)
+                {
+                    Borrowers.Remove(borrower);
+                    return;
+                }
+            }    
+        }
+
+        public List<string> ListBorrowers()
+        {
+            var borrowerStrings = new List<string>();
+            var indexer = 1;
+
+            foreach (var borrower in Borrowers)
+            {
+                var  borrowerString = indexer + ". " + borrower.Name + " - " + borrower.Amount + " zł";
+                indexer++;
+
+                borrowerStrings.Add(borrowerString);
+            }
+
+            return borrowerStrings;
+        }
+    }
+}
---
+++ APPC#Debtor/Debtor/DebtorApp.cs
@@ -0,0 +1,85 @@
+﻿using Debtor.Core;
+using System;
+
+namespace Debtor
+{
+    public class DebtorApp
+    {
+        public BorrowerManager BorrowerManager { get; set; } = new BorrowerManager();
+        public void IntroduceDeptorApp()
+        {
+            Console.WriteLine("Witaj w aplikacji Dłużnik");
+        }
+        public void AddBorrower()
+        {
+            Console.WriteLine("Podaj nazwe dłużnika, którego chcesz dodać do listy");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Podaj kwote długu");
+            var userAmount = Console.ReadLine();
+
+            if (decimal.TryParse(userAmount, out var ammountInDecimal))
+            {
+                BorrowerManager.AddBorrower(userName, ammountInDecimal);
+            }
+        }
+
+        public void DeleteBorrowe()
+        {
+            Console.WriteLine("Podaj nazwe dłużnika, którego chcesz usunąć z listy");
+            var userName = Console.ReadLine();
+
+           
+           BorrowerManager.DeleteBorrower(userName);
+            Console.WriteLine("Użytkownik usunięty");
+        }
+
+        public void ListBorrowers()
+        {
+            Console.WriteLine("Oto lista Twoich dłużników");
+            foreach (var borrower in BorrowerManager.ListBorrowers())
+            {
+                Console.WriteLine(borrower);
+            }
+        }
+
+        public void AskForAction()
+        {
+            Console.WriteLine("Podaj czynność, którą chcesz wykonać:");
+
+            var userInput = default(string);
+            while (/*userInput != "exit"*/ true)
+            {
+                Console.WriteLine("add - Dodawanie użytkownika");
+                Console.WriteLine("del - Usuwanie użytkownika");
+                Console.WriteLine("list - Wypisywanie listy dłużnika");
+                Console.WriteLine("exit - wyjście z programu");
+            }
+
+            userInput = Console.ReadLine();
+            userInput = userInput.ToLower(); //userInput na poczatku inaczej bedzie kopiowało
+
+            switch (userInput)
+            {
+                case "add":
+                    AddBorrower();
+                    break;
+                case "del":
+                    DeleteBorrowe();
+                    break;
+                case "list":
+                    ListBorrowers();
+                    break;
+                case "exit":
+                    return;
+                    default:
+                    Console.WriteLine("Podano złą wartość");
+                    break;
+            }
+
+        }
+
+    }
+
+    
+}
